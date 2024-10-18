5af3c83c89d91063bdfdcd35e6b8b87b725049af
Shahbaj <shahbaj.husen@neosoftmail.com>
2024-10-18 18:35:09 +0530

    initial commit - CI4

---
+++ app/Database/Migrations/18102024173500_Admin.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+// use \CodeIgniter\Database\Migration;
+
+class Admin extends \CodeIgniter\Database\Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'auto_increment' => true,
+                'null' => false,
+                'unsigned' => true
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'email' => [
+                'type' => 'TEXT',
+                'constraint' => '100',
+            ],
+            'phone' => [
+                'type' => 'TEXT',
+                'constraint' => '100',
+            ],
+            'password' => [
+                'type' => 'TEXT',
+                'constraint' => '100',
+            ],
+            'user_type' => [
+                'type'    => 'ENUM',
+                'constraint' => ['admin', 'user'], 
+                'default' => 'user',
+                'null'    => false,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('admin');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('admin');
+    }
+}
